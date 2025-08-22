cf134714107e383eaf27b712c25c4480a5639d8f
ailtonganem <132284290+ailtonganem@users.noreply.github.com>
2025-08-22 19:13:20 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// ====================================================================
+// ARQUIVO DE CONFIGURAÇÃO DO FIREBASE
+// ====================================================================
+// ATENÇÃO: ESTE ARQUIVO NÃO DEVE SER ENVIADO PARA O GITHUB.
+// Adicione a linha "firebase-config.js" ao seu arquivo .gitignore
+// ====================================================================
+
+// Configuração do seu novo projeto Firebase.
+// Estes dados são sensíveis e conectam sua aplicação ao backend do Firebase.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCytIG8h0hnFCAftDpTiJqEG6v40pNgmjo",
+  authDomain: "lara-3f96d.firebaseapp.com",
+  projectId: "lara-3f96d",
+  storageBucket: "lara-3f96d.firebasestorage.app",
+  messagingSenderId: "771300174449",
+  appId: "1:771300174449:web:f7b64de0f3e5260b6709d0"
+};
